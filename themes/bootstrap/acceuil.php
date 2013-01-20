2a74609be1a788ee39ca504eb2a6b5190f3207b4
Dussaucy Patryk <warzor_gamer@hotmail.fr>
2013-01-20 23:36:16 +0100

    Premier commit

---
+++ themes/bootstrap/acceuil.php
@@ -0,0 +1,48 @@
+<?php $this->inc('elements/header.php');?>
+
+
+<div class="row">
+		    <div class="span10">
+		        <div class="hero-unit">
+				 
+				   <?php
+		 $a = new Area ("Contenu");
+		 $a -> display($c);
+		 ?> 	
+				</div>
+		      
+		      <div class="row">
+		      	<div class="span5">
+		      		
+					 <?php
+		 $a = new Area ("Bas gauche");
+		 $a -> display($c);
+		 ?> 	
+		      	</div>	      	
+		      	<div class="span5">
+		      		
+					<?php
+		 $a = new Area ("Bas droite");
+		 $a -> display($c);
+		 ?> 	
+		      	</div>
+		      </div>
+		      
+		    </div>
+		    <div class="span2">
+		    	<div class="well well-small">
+					
+					<?php
+		 $a = new Area ("Lateral ");
+		 $a -> display($c);
+		 ?> 
+				</div>
+		    </div>
+	    </div>
+		 <div class="row">
+	    
+
+
+
+
+<?php $this->inc('elements/footer.php');?>
---
+++ themes/bootstrap/default.php
@@ -0,0 +1,25 @@
+<?php $this->inc('elements/header.php');?>
+
+<div class="row">
+		    <div class="span10">
+		        <div class="hero-unit">
+				  <h1>Bootstrap starter template</h1>
+				  <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+				  
+				  <a class="btn btn-primary btn-large">Plus d'infos</a>
+				</div>
+		      		    
+		    </div>
+		    <div class="span2">
+		    	<div class="well well-small">
+					<h3>Contenu latéral</h3>
+					<p>
+						Curabitur non felis in risus eleifend ultricies. Sed a tellus eros, a euismod ipsum.
+					</p>
+				</div>
+		    </div>
+	    </div>
+		 <div class="row">
+
+
+<?php $this->inc('elements/footer.php');?>
