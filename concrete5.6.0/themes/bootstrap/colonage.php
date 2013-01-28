1f4cd976e20b16525d56a6782de24abef2985ed5
Dussaucy Patryk <warzor_gamer@hotmail.fr>
2013-01-28 19:27:47 +0100

    4èm commit
    Création du fichier Colonnage.php

---
+++ concrete5.6.0/themes/bootstrap/colonage.php
@@ -0,0 +1,49 @@
+<?php $this->inc('elements/header.php');?>
+
+<div class="row">
+		    <div class="span4">
+		        <div class="hero-unit">
+				  <h1>Colone1</h1>
+				  <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+				  
+				  <a class="btn btn-primary btn-large">Plus d'infos</a>
+				</div>
+		      
+		      
+	    </div>
+		<div class="span4">
+		        <div class="hero-unit">
+				  <h1>Colone2</h1>
+				  <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+				  
+				  <a class="btn btn-primary btn-large">Plus d'infos</a>
+				</div>
+		      
+		      
+	    </div>
+		<div class="span4">
+		        <div class="hero-unit">
+				 <h1>Colone3</h1>
+				  <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+				  
+				  <a class="btn btn-primary btn-large">Plus d'infos</a>
+				</div>
+		      
+		      
+	    </div>
+ </div>
+ <div class="row">
+		    <div class="span12">
+		        <div class="hero-unit">
+				  <h1>Colone centrale</h1>
+				  <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+				  
+				  <a class="btn btn-primary btn-large">Plus d'infos</a>
+				</div>
+		      
+		      
+	    </div>
+		
+ </div>
+ 
+ <?php $this->inc('elements/footer.php');?>
